038fb2289c2c1910f5dfa6b8cf4b977404547501
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-17 15:32:04 +0700

    Menyelesaikan konflik merge antara generic-method dan generic-class

--- tpmodul5_2311104049/Program.cs
+++ tpmodul5_2311104049/Program.cs
@@ -2,6 +2,15 @@
 
 namespace tpmodul5_2311104049
 {
+    // Kelas Generic HaloGeneric
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
     // Kelas Generic DataGeneric
     class DataGeneric<T>
     {
@@ -22,11 +31,21 @@ namespace tpmodul5_2311104049
     {
         static void Main()
         {
+            HaloGeneric halo = new HaloGeneric();
+
+            // Input Nama
+            Console.WriteLine("=== Program Halo Generic Method ===");
+            Console.Write("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+            halo.SapaUser(nama);
+
+            // Input NIM
+            Console.WriteLine("=== Progra Halo Generic Class");
             Console.Write("Masukkan NIM Anda: ");
             string inputNIM = Console.ReadLine();
-
             DataGeneric<string> dataNIM = new DataGeneric<string>(inputNIM);
             dataNIM.PrintData();
+
             Console.ReadLine();
         }
     }
